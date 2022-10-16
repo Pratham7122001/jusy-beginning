68e61a4df5d60c824560fb70986590c0fb44afb7
Pratham7122001 <prathamgangwal7777@gmail.com>
2022-10-16 17:43:07 +0530

    Day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:classic/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 import 'pages/home_page.dart';
 
@@ -16,13 +18,17 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       //home: home_page(),
       themeMode: ThemeMode
-          .light, //if light ke jagah dark use karunga toh colors koi bhi dalo loi farak nhai padega
-      theme: ThemeData(primarySwatch: Colors.brown),
+          .light, //if light ke jagah dark use karunga toh colors koi bhi dalo loi farak nahi padega
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        fontFamily: GoogleFonts.lato().fontFamily,//lato doge toh niche latotexttheme hi deni padegi
+        primaryTextTheme: GoogleFonts.latoTextTheme() //isse kuch jyada changes nahi aare so use fontfamily
+        ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      //initialRoute: "/home",
       routes: {
-        "/":(context)=>home_page(),//error aayega bcoz in line 15 pehl se hi home define kiya hai and again in this line /use kiya jiska meaning hi home ke route karna hai so eithe line 15 rakho ya line 22   
-       // "/":(context)=>login_page(),
+        "/":(context)=>home_page(),//error aayega bcoz in line 15 pehl se hi home define kiya hai and again in this line / use kiya jiska meaning hi home ke route karna hai so either line 15 rakho ya line 22   
+        "/":(context)=>login_page(),
         "/login":(context)=>login_page(),
       
       },
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,10 +6,47 @@ class login_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child:Text("login_page"),
-        ),
-    );
-    
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/undraw_secure_login_pdn4.png",
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Toh kaise hai aap log");
+                      },
+                      child: Text("login"))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
